4bdf4201a728822f51189ae2b4c7a0d144ef106e
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-09-26 20:32:47 +0600

    exported props

---
+++ src/InputMask.svelte
@@ -0,0 +1,11 @@
+<script>
+
+  export let mask =  [];
+  export let showMask = false;
+  export let guide = true;
+  export let guideOnOutput = false;
+  export let keepCharPositions = true;
+  export let placeholder = '' ;
+  export let classes = '';
+
+</script>
